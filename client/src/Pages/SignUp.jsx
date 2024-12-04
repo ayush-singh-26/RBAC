6b3d5e70f3e212f19585e79ca4131ab53c6197f6
ayush-singh-225 <ayushsinghtemp@gmail.com>
2024-12-05 02:16:45 +0530

    vercel.json added

--- client/src/Pages/SignUp.jsx
+++ client/src/Pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { useForm } from 'react-hook-form';
 import axios from "axios";
 import { useNavigate } from 'react-router-dom';
-import { baseUrl } from "../Url";
+import { baseUrl } from "./Url";
 
 
 function SignUp() {
